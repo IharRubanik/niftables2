230d895296fafb1de3260fe0bdeaf4f7a09e9b12
IharRubanik <igor.rubanik@gmail.com>
2022-05-25 17:54:57 +0400

    asdasaas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let body = document.querySelector(".body");
+let process = document.querySelector(".process");
 
 let arrActive = ([] = document.querySelectorAll(
   ".header-button, .header-content, .header-name, body"
@@ -10,3 +11,37 @@ document.querySelector(".header-button").onclick = function () {
   }
 };
 
+
+const gridItems = document.querySelectorAll('.process-parent__item');
+
+window.addEventListener('scroll', function () {
+    const sticky = document.querySelector('.process-parent'),
+        scroll = window.pageYOffset - sticky.offsetTop,
+        height = sticky.scrollHeight;
+        
+    let i = 0;
+
+    scroll < (1 * (height / 6) - 500) ? i = 0 :
+    scroll < (2 * (height / 6) - 500) ? i = 1 :
+    scroll < (3 * (height / 6) - 500) ? i = 2 :
+    scroll < (4 * (height / 6) - 500) ? i = 3 :
+    scroll < (5 * (height / 6) - 500) ? i = 4 : i = 5;
+
+    for (let j = 0; j < gridItems.length; j++){
+        gridItems[j].classList.remove('view');
+        gridItems[j].classList.remove('active');
+        if (j <= i) {
+            gridItems[j].classList.add('view');
+        }
+    }
+    gridItems[i].classList.add('active')
+});
+
+function reScroll(num, e){
+    const sticky = document.querySelector('.process-parent'),
+        scroll = window.pageYOffset + sticky.offsetTop,
+        height = sticky.scrollHeight,
+        targ = e.target;
+        
+    window.scrollTo(0, sticky.offsetTop + (num * (height / 6) - (height / 6 / 3)))    
+}
