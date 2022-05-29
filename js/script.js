32fca3449d3f152951d26cf14ba8f88c074e0159
IharRubanik <igor.rubanik@gmail.com>
2022-05-29 22:58:02 +0400

    okllk/ml

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ document.querySelector(".header-button").onclick = function () {
   }
 };
 
+// let slider = document.getElementsByClassName("features__slider-container");
 let slides = document.getElementsByClassName("features__slider-item");
 let dots = document.getElementsByClassName("slider-dots_item");
 let active;
@@ -71,3 +72,53 @@ function showSlides(n) {
   }
   dots[slideIndex - 1].className += " active";
 }
+
+
+
+
+
+// Вешаем на прикосновение функцию handleTouchStart
+document.addEventListener('touchstart', handleTouchStart, false);  
+// А на движение пальцем по экрану - handleTouchMove      
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+          plusSlide()
+          console.log("ыдоваылдвао");
+            /* left swipe */ 
+        } else {
+          console.log("ыдоваылываываыаывдвао");
+            /* right swipe */
+            minusSlide()
+        }                       
+    } else { // Это вам, в общем-то, не надо, вы ведь только влево-вправо собираетесь двигать
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
