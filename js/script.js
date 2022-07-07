3bb4d9dc77eebff6b445d5f181e65205dea67e7f
IharRubanik <igor.rubanik@gmail.com>
2022-07-07 10:11:43 +0400

    js

--- js/script.js
+++ js/script.js
@@ -301,8 +301,9 @@ var xDown = null;
 var yDown = null;                                                    
 
 function handleTouchStart(evt) {                                         
-    xDown = evt.touches[0].clientX;                                      
-    yDown = evt.touches[0].clientY;                                      
+    xDown = evt.changedTouches[0].clientX;                                      
+    yDown = evt.changedTouches[0].clientY;        
+                                  
 };                                                
 
 function handleTouchMove(evt) {
@@ -310,8 +311,8 @@ function handleTouchMove(evt) {
         return;
     }
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
+    var xUp = evt.changedTouches[0].clientX;                                    
+    var yUp = evt.changedTouches[0].clientY;
 
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp;
