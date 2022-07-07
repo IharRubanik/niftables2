9d4f974443337004d9366f057652291959e79b43
Ernest Brozinski <ernestbrozinski@gmail.com>
2022-07-07 12:11:58 +0400

    check

--- js/script.js
+++ js/script.js
@@ -322,7 +322,7 @@ function handleTouchMove(evt) {
 
         if ( xDiff > 0 ) {
           /* left swipe */ 
-          alert(evt.path[0])
+          alert('left swipe', evt.path[0])
           if (evt.path[0].id === 'c1') {
             document.querySelector('.features__slider.slider1 .slider__button .button.next').click();
           }
@@ -337,7 +337,7 @@ function handleTouchMove(evt) {
           }
         } else {
           /* right swipe */
-          alert(evt.path)
+          alert('right swipe'. evt.path[0].id)
           if (evt.path[0].id === 'c1') {
             document.querySelector('.features__slider.slider1 .slider__button .button.prev').click();
           }
