e0bf16f3080f71654acf3da1cfb217c31572ce5d
Ernest Brozinski <ernestbrozinski@gmail.com>
2022-07-08 15:36:38 +0400

    check

--- js/script.js
+++ js/script.js
@@ -304,7 +304,7 @@ canvas3.addEventListener('touchstart', handleTouchStart, false);
 // А на движение пальцем по экрану - handleTouchMove      
 canvas1.addEventListener('touchmove', handleTouchMove, false);
 canvas2.addEventListener('touchmove', handleTouchMove, false);
-canvas3.addEventListener('touchend', handleTouchMove, false);
+canvas3.addEventListener('touchmove', handleTouchMove, false);
 // Вешаем на прикосновение функцию handleTouchEnd
 canvas1.addEventListener('touchend', handleTouchEnd, false);  
 canvas2.addEventListener('touchend', handleTouchEnd, false);  
@@ -328,7 +328,7 @@ function handleTouchMove(evt) {
         return;
     }
 
-    if (Math.abs(startSwipeY - endSwipeY) >= 100) {
+    if (Math.abs(startSwipeY - endSwipeY) >= 40) {
       this.classList.remove('touch');
     } else {
       this.classList.add('touch');
